becf9d257a3a5bedf044742e805316b55152c03d
miloszb <milosz.b@gmail.com>
2017-02-10 14:28:33 +0100

    work in progress

---
+++ src/Connection.php
@@ -0,0 +1,35 @@
+<?php
+
+class Connection
+{
+    private $mysqli;
+    public $insert_id;
+
+    public function __construct()
+    {
+        require_once 'config.php';
+
+        $this->mysqli = new mysqli($dbServer, $dbUser, $dbPassword, $dbName);
+
+        if($this->mysqli->connect_error)
+        {
+            die('Databse connection error: '.$this->mysqli->connect_error);
+        } else
+        {
+            $this->mysqli->set_charset("utf8");
+            return true;
+        }
+    }
+
+    public function query($sql)
+    {
+        $result = $this->mysqli->query($sql);
+
+        if($result == false)
+        {
+            die('Query error: '.$this->mysqli->error);
+        } else {
+            return $result;
+        }
+    }
+}
