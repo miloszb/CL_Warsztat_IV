d6d2ca04a2a7d6d901128c934272faeeea865b0e
miloszb <milosz.b@gmail.com>
2017-02-10 17:28:48 +0100

    fix UserTest

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -3,26 +3,39 @@ use PHPUnit\Framework\TestCase;
 
 class UserTest extends TestCase
 {
+    private $testUser;
+    
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->testUser = new User('Józek', 'Wózek', 'jwozek@org.org', 'bumbum123');
+    }
     public function testConstruct()
     {
-        $tstU = new User('Józek', 'Wózek', 'jwozek@org.org', 'bumbum123');
-        $this->assertInstanceOf(User::class, $tstU);    
-        $this->assertEquals('Józek', $tstU->getName());
-        $this->assertEquals('Wózek', $tstU->getSurname());
-        $this->assertEquals('jwozek@org.org', $tstU->getEmail());
-        $this->assertEquals('bumbum123', $tstU->getPassword());
+        $this->assertInstanceOf(User::class, $this->testUser);    
+        $this->assertEquals('Józek', $this->testUser->getName());
+        $this->assertEquals('Wózek', $this->testUser->getSurname());
+        $this->assertEquals('jwozek@org.org', $this->testUser->getEmail());
+        $this->assertEquals('bumbum123', $this->testUser->getPassword());
     }
     public function testAddToDb()
     {
-        $tstU = new User('Józek', 'Wózek', 'jwozek5@org.org', 'bumbum123');
-        $tstU->save();
-        $this->assertTrue($tstU->getId() != -1);
+        $this->testUser->save();
+        $this->assertTrue($this->testUser->getId() > 0);
     }
     public function testLogin()
     {
-        $tstU = User::login('jwozek@org.org', 'wrongpassword');
-        $this->assertFalse($tstU);
-        $tstU = User::login('jwozek@org.org', 'bumbum123');
-        $this->assertEquals('Józek', $tstU->getName());
+        $this->testUser->save();
+        $loggedUserBad = User::login('jwozek@org.org', 'wrongpassword');
+        $this->assertFalse($loggedUserBad);
+        $loggedUserGood = User::login('jwozek@org.org', 'bumbum123');
+        $this->assertEquals('Józek', $loggedUserGood->getName());
+    }
+    protected function tearDown()
+    {
+        $db = new Connection();
+        $sql = 'TRUNCATE TABLE user';
+        $db->query($sql);
+        parent::tearDown();
     }
 }
