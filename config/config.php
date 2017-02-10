e313d98082e7cea833b04ac37b01cf099fd4dd69
peterbuilder <peterbuilder@github.com>
2017-02-10 15:08:44 +0100

    Create class ProductTest

---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: piotr
+ * Date: 10.02.17
+ * Time: 15:01
+ */
---
+++ tests/ProductTest.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: piotr
+ * Date: 10.02.17
+ * Time: 15:06
+ */
+
+use PHPUnit\Framework\TestCase;
+
+class ProductTest extends TestCase
+{
+
+}
