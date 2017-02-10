d5526e3bf82d18585385bb5e08bab1a19c23e547
peterbuilder <peterbuilder@github.com>
2017-02-10 16:54:38 +0100

    Add tests

--- tests/ProductTest.php
+++ tests/ProductTest.php
@@ -11,5 +11,38 @@ use PHPUnit\Framework\TestCase;
 
 class ProductTest extends TestCase
 {
+    private $product;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->product = new Product();
+    }
+
+    public function testCreateObject(){
+        $this->assertInstanceOf(Product::class, $this->product);
+    }
+
+    function testSetName()
+    {
+        $this->product->setName('Piotr');
+        $name = $this->product->getName();
+        $this->assertEquals('Piotr', $name);
+    }
+
+    function testSetPrice()
+    {
+        $this->product->setPrice(100);
+        $name = $this->product->getPrice();
+        $this->assertEquals(100, $name);
+    }
+
+    function testSetDescription()
+    {
+        $this->product->setDescription('Opis');
+        $name = $this->product->getDescription();
+        $this->assertEquals('Opis', $name);
+    }
+
 
 }
