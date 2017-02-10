eed67bf94c55d63e9b4930ef1330ed93439dff2f
peterbuilder <peterbuilder@github.com>
2017-02-10 14:49:30 +0100

    Create getters and setters

--- src/Product.php
+++ src/Product.php
@@ -8,5 +8,65 @@
  */
 class Product
 {
+    private $id;
+    private $name;
+    private $price;
+    private $description;
+    private $stock;
+
+    public function __construct()
+    {
+        $this->id = -1;
+        $this->name = '';
+        $this->price = '';
+        $this->description = '';
+        $this->stock = '';
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    public function setPrice($price)
+    {
+        $this->price = $price;
+    }
+
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    public function getStock()
+    {
+        return $this->stock;
+    }
+
+    public function setStock($stock)
+    {
+        $this->stock = $stock;
+    }
+
 
 }
