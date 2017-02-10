c33f1ac73609f10e81a8ca3a2b83a602097a7d3a
peterbuilder <peterbuilder@github.com>
2017-02-10 16:54:21 +0100

    Add getters and setters

--- src/Product.php
+++ src/Product.php
@@ -23,49 +23,81 @@ class Product
         $this->stock = '';
     }
 
-    public function getId()
-    {
-        return $this->id;
-    }
 
-    public function getName()
-    {
-        return $this->name;
-    }
-
-    public function setName($name)
+    public function setName(string $name)
     {
         $this->name = $name;
     }
 
-    public function getPrice()
-    {
-        return $this->price;
-    }
-
-    public function setPrice($price)
+    public function setPrice(string $price)
     {
         $this->price = $price;
     }
 
-    public function getDescription()
-    {
-        return $this->description;
-    }
-
-    public function setDescription($description)
+    public function setDescription(string $description)
     {
         $this->description = $description;
     }
 
-    public function getStock()
+    public function setStock(string $stock)
+    {
+        $this->stock = $stock;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getPrice(): string
+    {
+        return $this->price;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function getStock(): string
     {
         return $this->stock;
     }
 
-    public function setStock($stock)
+    public function saveToDB(Connection $connection)
     {
-        $this->stock = $stock;
+        if($this->id == -1) {
+            $sql = "INSERT INTO product (id, name, price, description, stock)
+                    VALUES ('$this->id', '$this->name', '$this->price', '$this->description', '$this->stock')";
+            $result = $connection->query($sql);
+
+            if($result == true) {
+                $this->id = $connection->insert_id;
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            $sql = "UPDATE product 
+                    SET id='$this->id',
+                        name='$this->name',
+                        price='$this->price',
+                        description='$this->description',
+                        stock='$this->stock'
+                    WHERE id=$this->id";
+            $result = $connection->query($sql);
+
+            if($result == true) {
+                return true;
+            } else {
+                return false;
+            }
+        }
     }
 
 
