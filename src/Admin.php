a918d16ef2e990f5fe319c061edea45b744ff01a
peterbuilder <peterbuilder@github.com>
2017-02-12 21:18:21 +0100

    Add getters and setters

--- src/Admin.php
+++ src/Admin.php
@@ -8,7 +8,54 @@
  */
 class Admin
 {
+    private $id;
     private $name;
     private $email;
     private $password;
+
+    public function __construct()
+    {
+        $this->id = -1;
+        $this->name = '';
+        $this->email = '';
+        $this->password = '';
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name)
+    {
+        $this->name = $name;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email)
+    {
+        $this->email = $email;
+    }
+
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password)
+    {
+        $this->password = $password;
+    }
+
+
+
 }
